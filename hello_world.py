964dda8ec9c0a53f169271e49a8ee98a63319271
shubhambakshilangara <105755366+shubhambakshilangara@users.noreply.github.com>
2022-05-17 17:58:06 -0700

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+'''
+Name: Shubham Bakshi
+ID
+'''
+import math
+
+print("Hello World")
+'''
+name = ''
+name = input('enter your name ')
+course = input('enter your course ')
+print('Hello',name)
+print('Hello '+name )
+print(f'welcome {name}/{name.upper()}/{name.title()} to this {course} ')
+'''
+x=math.pi
+print(f'here is {x:.9f}')
